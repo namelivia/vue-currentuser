1e9b3376953cd4bb376fdbfe64512a4bfc693e4b
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-04-02 17:04:20 +0200

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,25 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import packageJson from "./package.json";
 
 export default {
   input: "src/index.js",
   output: [
     {
       format: "cjs",
-      file: packageJson.main,
+      file: "dist/index.js",
       sourcemap: true
     },
     {
       format: "esm",
-      file: packageJson.module,
+      file: "dist/index.esm.js",
       sourcemap: true
     }
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
-    commonjs()
+    commonjs(),
   ]
 };
 
